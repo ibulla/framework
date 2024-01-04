0ce17c18285c6cf5ed8bdb6497cfd7fbba76b299
Marco Spitzbarth <bulla@ibulla.com>
2024-01-04 17:04:37 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,4 @@
 <?php
-//eg: https:ibulla.com/..
-//header("Location: (your path) /framework/admin/admin_login.php");
+header("Location: $_SERVER['HTTP_HOST']."/framework/admin/admin_login.php");
 die();
 ?>
